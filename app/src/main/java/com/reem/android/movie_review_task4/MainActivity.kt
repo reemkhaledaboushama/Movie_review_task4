aa027b201ea50226e9dbc27debac73f154829e01
reemk <reemkhaledaboushama@gmail.com>
2020-08-29 21:40:37 +0300

    setting the repository of project file on github

---
+++ app/src/main/java/com/reem/android/movie_review_task4/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.reem.android.movie_review_task4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
