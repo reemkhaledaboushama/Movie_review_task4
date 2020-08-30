9f0583da1116b48ddeb7c10a37ba84512d9eeea4
olaalnajjar <69318042+olaalnajjar@users.noreply.github.com>
2020-08-30 23:07:47 +0200

    println message

--- app/src/main/java/com/reem/android/movie_review_task4/MainActivity.kt
+++ app/src/main/java/com/reem/android/movie_review_task4/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("hello team i think my github is finally working! yay :)")
     }
 }
