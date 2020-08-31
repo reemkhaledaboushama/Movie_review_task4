de2dd2436fb25147e1b76f37216bd730aff1f42a
olaalnajjar <69318042+olaalnajjar@users.noreply.github.com>
2020-09-01 00:39:16 +0200

    Movie data class

---
+++ app/src/main/java/com/reem/android/movie_review_task4/APIMovie.kt
@@ -0,0 +1,9 @@
+package com.reem.android.movie_review_task4
+
+
+
+interface APIMovie {
+//https://api.themoviedb.org/3/movie/popular?api_key=d78d306988042530d99e166c3cb00017&language=en-US&page=1
+    fun getMovie()
+
+}
