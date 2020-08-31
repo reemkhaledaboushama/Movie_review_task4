a0f728f39f76ed7c377fd4178aac4d159ddfe848
Sherif-Ahmed17 <sherifnaiem2000@gmail.com>
2020-08-31 16:07:21 +0200

    Hi printed

--- app/src/main/java/com/reem/android/movie_review_task4/MainActivity.kt
+++ app/src/main/java/com/reem/android/movie_review_task4/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("hello team i think my github is finally working! yay :)")
+        print("Hi")
     }
 }
