2a63b3097dcf6ce987c4052eef9b7aa39d33a651
Yomna18 <yamona.hus@gmail.com>
2020-09-03 01:40:19 +0200

    test33

--- app/src/main/java/com/reem/android/movie_review_task4/Movie.kt
+++ app/src/main/java/com/reem/android/movie_review_task4/Movie.kt
@@ -1,10 +1,10 @@
 package com.reem.android.movie_review_task4
 
 data class Movie (val popularity :Double,
-                  val poster_path: String,
-                  val adult :Boolean,
-                  val original_language :String = "en",
-                  val original_title :String ,
-                  val title :String,
-                  val overview :String ,
-                  val release_date : String)
+val poster_path: String,
+val adult :Boolean,
+val original_language :String = "en",
+val original_title :String ,
+val title :String,
+val overview :String ,
+val release_date : String)
